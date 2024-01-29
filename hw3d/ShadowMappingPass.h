5e3d0c7f85cb4771c5f8ce2b4d8176fc0402f830
mliu <mliu@newpark.com>
2024-01-29 11:42:21 -0600

    Fix the solution rebuild error as error C2102: '&' requires l-value

--- hw3d/ShadowMappingPass.h
+++ hw3d/ShadowMappingPass.h
@@ -70,8 +70,8 @@ namespace Rgph
 		void Execute( Graphics& gfx ) const noxnd override
 		{
 			using namespace DirectX;
-
-			const auto pos = XMLoadFloat3( &pShadowCamera->GetPos() );
+			auto temp = pShadowCamera->GetPos();
+			const auto pos = XMLoadFloat3( &temp );
 
 			gfx.SetProjection( XMLoadFloat4x4( &projection ) );
 			for( size_t i = 0; i < 6; i++ )
